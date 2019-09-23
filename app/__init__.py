ef37404be44b086be5ebe671c6ab3c07552b4a45
Mohamed Ragaiy Saleh <mohamed.ragaiy.saleh@gmail.com>
2019-09-23 14:13:07 +0300

    Fixed Recaptcha Error

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app.config["DEBUG"] = True
 app.config["TESTING"] = True
 # app.config["USE_SESSION_FOR_NEXT"] = True
 
-print(">>> app line 15")
+
 import os
 
 app.secret_key = os.urandom(24)
@@ -27,26 +27,21 @@ else:
     app.config["MONGODB_DB"] = DB_NAME
     app.config["MONGODB_PORT"] = PORT
     app.config["MONGODB_HOST"] = HOST
-print(">>> app line 30")
+
 
 from common.db import db
 
 db.init_app(app)
 
-try:
-    # TODO should use environment values <DONE?>
-    from x.recaptcha import RECAPCHA_SITE_KEY, RECAPCHA_SECRET_KEY
 
-    app.config["RECAPTCHA_PUBLIC_KEY"] = os.environ.get("RECAPCHA_SITE_KEY", RECAPCHA_SITE_KEY)
-    app.config["RECAPTCHA_PRIVATE_KEY"] = os.environ.get("RECAPCHA_SECRET_KEY", RECAPCHA_SECRET_KEY)
-except:
-    pass
+if "RECAPTCHA_SITE_KEY" in os.environ and "RECAPTCHA_SECRET_KEY" in os.environ:
+    app.config["RECAPTCHA_PUBLIC_KEY"] = os.environ["RECAPTCHA_SITE_KEY"]
+    app.config["RECAPTCHA_PRIVATE_KEY"] = os.environ["RECAPTCHA_SECRET_KEY"]
 
-print(">>> app line 45")
+
 from api import api_bp
 from website import site_bp
 
 app.register_blueprint(api_bp, url_prefix="/api")
 app.register_blueprint(site_bp)
 
-print(">>> app line 52")
