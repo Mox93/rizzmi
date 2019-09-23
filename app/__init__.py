3acaf302d7b634842b794f8100b36d648d5435d5
Mohamed Ragaiy Saleh <mohamed.ragaiy.saleh@gmail.com>
2019-09-23 18:08:11 +0300

    Changed DB configs

--- app/__init__.py
+++ app/__init__.py
@@ -15,22 +15,14 @@ app.config["TESTING"] = True
 
 import os
 
-app.secret_key = os.urandom(24)
+app.config["SECRET_KEY"] = os.urandom(24)
 
 app.config["RECAPTCHA_PUBLIC_KEY"] = os.environ["RECAPTCHA_SITE_KEY"]
 app.config["RECAPTCHA_PRIVATE_KEY"] = os.environ["RECAPTCHA_SECRET_KEY"]
 
-
-from common.db import DB_NAME, PORT, HOST
-
-if "MONGOLAB_MAROON_URI" in os.environ:
-    print(">>> MONGOLAB_MAROON_URI was found")
-    app.config['MONGODB_SETTINGS'] = {'db': DB_NAME,
-                                      'host': os.environ["MONGOLAB_MAROON_URI"]}
-else:
-    app.config["MONGODB_DB"] = DB_NAME
-    app.config["MONGODB_PORT"] = PORT
-    app.config["MONGODB_HOST"] = HOST
+app.config["MONGODB_DB"] = os.environ["DB_NAME"]
+app.config["MONGODB_HOST"] = os.environ["MONGOLAB_MAROON_URI"]
+app.config['MONGODB_CONNECT'] = False
 
 
 from common.db import db
