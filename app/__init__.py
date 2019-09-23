e0d72d27b377b2d9c39bef4ced93e95fa8ec8775
Mohamed Ragaiy Saleh <mohamed.ragaiy.saleh@gmail.com>
2019-09-23 03:28:30 +0300

    Trying an new env val for mlab

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,7 @@ from common.db import DB_NAME, PORT, HOST
 
 if "MONGODB_URI" in os.environ:
     app.config['MONGODB_SETTINGS'] = {'db': DB_NAME,
-                                      'host': os.environ["MONGODB_URI"]}
+                                      'host': os.environ["MONGOLAB_TEAL"]}
 else:
     app.config["MONGODB_DB"] = DB_NAME
     app.config["MONGODB_PORT"] = PORT
@@ -34,7 +34,7 @@ from common.db import db
 db.init_app(app)
 
 try:
-    # TODO should use environment values
+    # TODO should use environment values <DONE?>
     from x.recaptcha import RECAPCHA_SITE_KEY, RECAPCHA_SECRET_KEY
 
     app.config["RECAPTCHA_PUBLIC_KEY"] = os.environ.get("RECAPCHA_SITE_KEY", RECAPCHA_SITE_KEY)
