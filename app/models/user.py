bfc1dffd844734f60bea120ea9d1a6982ba1d1ef
Mohamed Ragaiy Saleh <mohamed.ragaiy.saleh@gmail.com>
2019-10-16 19:35:50 +0300

    Added a homepage image

--- app/models/user.py
+++ app/models/user.py
@@ -3,20 +3,19 @@ from flask_login import UserMixin
 from bson import ObjectId
 
 
-class DeveloperModel(UserMixin, ExtendedDocument):
-    meta = {'collection': 'developers'}
+class UserModel(UserMixin, ExtendedDocument):
+    meta = {'collection': 'users',
+            'allow_inheritance': True}
 
     username = db.StringField(required=True, max_length=25, unique=True)
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True, max_length=80)
-    session_id = db.ObjectIdField(unique=True, defult=ObjectId)
+    session_id = db.ObjectIdField(required=True, unique=True, default=ObjectId)
 
     def get_id(self):
         return str(self.session_id)
 
     def clean(self):
-        if not self.session_id:
-            self.session_id = ObjectId()
 
         if isinstance(self.username, str):
             self.username = self.username.lower()
@@ -31,4 +30,10 @@ class DeveloperModel(UserMixin, ExtendedDocument):
     @classmethod
     def find_by_email(cls, email):
         return cls.objects(email=email.lower()).first()
+
+
+class DeveloperModel(UserModel):
+    pass
+
+
 
